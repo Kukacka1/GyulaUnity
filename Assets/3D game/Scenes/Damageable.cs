a2ac3168f12a1b30e56d88806fbf86f9725bfd44
Gyula <gyulakalman1@gmail.com>
2023-12-08 13:58:59 +0100

    StarterAssets added

--- Assets/3D game/Scenes/Damageable.cs
+++ Assets/3D game/Scenes/Damageable.cs
@@ -1,21 +1,43 @@
+using TMPro;
 using UnityEngine;
 
 public class Damageable : MonoBehaviour
 {
     [SerializeField] int startHP = 100;
+    [SerializeField] TMP_Text uiText;
+
+    void Start()
+    {
+        UpdateUI();
+
+    }
     public void Damage(int damage)
     {
 
+        startHP -= damage;
+        startHP = Mathf.Max(startHP, 0);
+
 
         Debug.Log("Áucs!!!!!");
-
-     
-        startHP -= damage;
+        UpdateUI();
 
         if (startHP <= 0)
         {
 
-            Debug.Log("GameOver");
+            Debug.Log("Game Over");
+
+            PlayerMoveing pm = GetComponent<PlayerMoveing>();
+            pm.enabled = false;
+
+        }
+    }
+
+    void UpdateUI()
+    {
+        if (uiText != null)
+        {
+
+            uiText.text = startHP.ToString();
 
         }
     }
